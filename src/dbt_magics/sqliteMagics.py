10e1a41d10d97b772bde7da466cc45005ddee396
Anton Enns <anton.enns@outlook.de>
2024-05-11 09:26:41 +0200

    sqlite wip

--- src/dbt_magics/sqliteMagics.py
+++ src/dbt_magics/sqliteMagics.py
@@ -4,25 +4,68 @@ from time import time
 import pandas as pd
 from IPython.core import display, magic_arguments
 from IPython import get_ipython
-from IPython.core.magic import Magics, cell_magic, magics_class
+from IPython.core.magic import Magics, line_cell_magic, magics_class
 from jinja2 import Environment, Template, meta
 
 from dbt_magics.datacontroller import DataController, prStyle
 from dbt_magics.dbt_helper import dbtHelper
 
 
-# class Adapter(dbtHelper):
-#     def __init__(self, profile_name="gcp_dwh", target='prod'):
-#         super().__init__(profile_name=profile_name, target=target)
-        
-#     def ref(self, table_name):
-#         return f'main."{table_name}"'
+class SQLiteDataController(DataController):
+    def __init__(self):
+        self.dbt_helper = dbtHelperAdapter(profile_name=None, target='prod')
+        super().__init__(r"%%sqlity")
 
-#     def source(self, source_name, table_name):
-#         SOURCES, _ = self._sources_and_models()
-#         database = [database for database in SOURCES if source_name==database['name']][0]
-#         schema = database['schema']
-#         return f'{schema}."{table_name}"'
+    """
+    Implemented Abstract methods
+    """
+    def get_projects(self):
+        return ['project']
+
+    def get_datasets(self, database):
+        
+        return list(self.dbt_helper.profile_config['schemas_and_paths'].keys())
+
+    
+    def get_tables(self, database):
+        if database:
+            return sorted(self.get_data(f'select * from {database}.sqlite_master')['name'].values)
+        else: 
+            return []
+    
+    def get_columns(self, table):
+        columns = self.get_data(f"PRAGMA {self.wg_database.value}.table_info({table});")[['name', 'type']].values
+        return columns
+
+    """ 
+    Additional methods 
+    """
+    def get_data(self, statement, dataset='main'):
+        return self.dbt_helper.run_query(
+                            sql_statement=statement,
+                            main_database=self.dbt_helper.profile_config['schemas_and_paths']['main'],
+                            extensions=self.dbt_helper.profile_config['extensions'],
+                            schemas_and_paths=self.dbt_helper.profile_config['schemas_and_paths'],
+                            verbose=False
+        )
+
+    # Print the SQL statement to the output widget when the button is clicked
+    def on_button_clicked(self, x, star=False):
+        f = lambda name, dtype:  f'{name} {prStyle.BLUE}-- {dtype}{prStyle.RESET}'
+        with self.output:
+            self.output.clear_output()
+            part_string = f"\n{prStyle.MAGENTA}LIMIT{prStyle.RESET} {prStyle.CYAN}100{prStyle.RESET}"
+            if star:
+                cols = '*'
+            else:
+                cols = "\n    , ".join([f(i.check.description, i.lab.value) for i in self.check_boxes if i.check.value])
+            
+            output_string = f'{prStyle.RED}{self.lineMagicName}{prStyle.RESET}\n{prStyle.MAGENTA}SELECT{prStyle.RESET}\n    {cols} \n{prStyle.MAGENTA}FROM{prStyle.RESET} {prStyle.GREEN}"{self.wg_database.value}"."{self.wg_tables.value}"{prStyle.RESET}{part_string}'
+
+            if self.includeLeadingQuotesInCellMagic:
+                print(output_string)
+            else:
+                print(output_string.replace('"', ''))
 
 
 class dbtHelperAdapter(dbtHelper):
@@ -42,7 +85,7 @@ class dbtHelperAdapter(dbtHelper):
     
 
 
-    def run_query(self, sql_statement, main_database, extensions=[], schemas_and_paths=None):
+    def run_query(self, sql_statement, main_database, extensions=[], schemas_and_paths=None, verbose=True):
         with sql.connect(main_database) as conn:
             cursor = conn.cursor()
             conn.enable_load_extension(True)
@@ -68,8 +111,7 @@ class dbtHelperAdapter(dbtHelper):
                 
                 df = None
             duration = time()-start
-            print(f'{prStyle.GREEN}Execution time: {int(duration//60)} min. - {duration%60:.2f} sec.')
-            # print(f"{prStyle.GREEN}{TotalExecutionTimeInMillis/1000:.3f} sec. {prStyle.RESET}| {prStyle.MAGENTA}{DataScannedInBytes:.3f} MB scanned {prStyle.RESET}| {prStyle.RED}{PriceInDollar:3.5f} ${prStyle.RESET}")
+            if verbose: print(f'{prStyle.GREEN}Execution time: {int(duration//60)} min. - {duration%60:.2f} sec.')
             return df
     
 
@@ -77,14 +119,14 @@ class dbtHelperAdapter(dbtHelper):
 class SQLMagics(Magics):
     pd.set_option('display.max_columns', None)
 
-    @cell_magic
+    @line_cell_magic
     @magic_arguments.magic_arguments()
     @magic_arguments.argument('--n_output', '-n', default=5, help='')
     @magic_arguments.argument('--dataframe', '-df', default="df", help='The variable to return the results in.')
     @magic_arguments.argument('--parser', '-p', action='store_true', help='Translate Jinja.')
     @magic_arguments.argument('--profile', default=None, help='')
     @magic_arguments.argument('--target', default='prod', help='')
-    def sqlity(self, line, cell):
+    def sqlity(self, line, cell=None):
         """
 ---------------------------------------------------------------------------
 %%athena
@@ -103,7 +145,7 @@ SELECT * FROM {{ ref('table_in_dbt_project') }}
 ---------------------------------------------------------------------------
 """
         if cell is None:
-            dc = AthenaDataController()
+            dc = SQLiteDataController()
             return dc()
         else:        
             args = magic_arguments.parse_argstring(self.sqlity, line)
